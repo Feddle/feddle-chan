4eb1ca5427034de805c2ce2572d4865a8ebe58e9
Feddle <sepptee@outlook.com>
2018-10-30 13:02:34 +0200

    feat: added helmetjs

--- server.js
+++ server.js
@@ -9,11 +9,40 @@ var apiRoutes         = require("./routes/api.js");
 var fccTestingRoutes  = require("./routes/fcctesting.js");
 var runner            = require("./test-runner");
 
+const helmet = require("helmet");
+
 var app = express();
 
 app.use("/public", express.static(process.cwd() + "/public"));
 
 app.use(cors({origin: "*"})); //For FCC testing purposes only
+
+
+let ninetyDaysInMilliseconds = 90*24*60*60*1000;
+app.use(helmet({
+  frameguard: "same-origin",
+  hidePoweredBy: {setTo: "PHP 4.2.0"},
+  xssFilter: true,
+  noSniff: true,
+  ieNoOpen: true,  
+  hsts: {maxAge: ninetyDaysInMilliseconds, force: true},
+  dnsPrefetchControl: true,
+  referrerPolicy: "same-origin",
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ["'self'", "'unsafe-inline'"],
+      fontSrc: ["'self'"],
+      imgSrc: ["'self'"],
+      scriptSrc: ["'self'", "'unsafe-inline'", "https://code.jquery.com"],
+      objectSrc: ["'none'"],
+      frameAncestors: ["'none'"],
+      baseUri: ["'none'"],
+      formAction: ["'self'"],
+      upgradeInsecureRequests: true,
+    }
+  }
+}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
