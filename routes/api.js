4b11837a2f9f3d4b4da2d896b3723411de570c81
Feddle <sepptee@outlook.com>
2018-10-30 16:21:45 +0200

    feat: added empty routes

--- routes/api.js
+++ routes/api.js
@@ -5,8 +5,32 @@ var expect = require("chai").expect;
 
 module.exports = function (app) {
   
-  app.route("/api/threads/:board");
+  app.route("/api/threads/:board")
+    .get((req, res) => {
+
+    })
+    .post((req, res) => {
+      
+    })
+    .put((req, res) => {
+      
+    })
+    .delete((req, res) => {
+      
+    });
     
-  app.route("/api/replies/:board");
+  app.route("/api/replies/:board")
+    .get((req, res) => {
+
+    })
+    .post((req, res) => {
+    
+    })
+    .put((req, res) => {
+    
+    })
+    .delete((req, res) => {
+    
+    });
 
 };
